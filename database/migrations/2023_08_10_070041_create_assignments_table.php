cdc47e49a06e3cd0060cbebd68770744cfcc0107
Sanadhi2002 <sanadhi2002@gmail.com>
2023-08-16 12:46:00 +0530

    assignments

---
+++ database/migrations/2023_08_10_070041_create_assignments_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        
+        Schema::create('assignments', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('assignment_name');
+            $table->foreignId('subject_id')->constrained('subjects');
+            $table->datetime('due_date');
+            $table->string('description');
+            $table->string('file');
+            $table->foreignId('teacher_id')->constrained('users')->nullable();
+            $table->datetime('upload_date');
+            
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('assignments');
+    }
+};
