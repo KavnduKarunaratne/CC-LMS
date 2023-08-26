2930ccba8ad8bc5df93f59aff4d19b78ccf68800
Sanadhi2002 <sanadhi2002@gmail.com>
2023-08-26 08:37:46 +0530

    fixing bug

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -17,164 +17,139 @@ class UserController extends Controller
 
     public function Role(){
         $roles = Role::all();
-        return view('register',compact('roles'));
+        return view('register', compact('roles'));
     }
 
-    public function User(){  //role id is 2 for management 
-        $user = User::where('is_archived',0)->get();
+    public function User(){  
+        $user = User::where('is_archived', 0)->get();
         $archivedUsers = User::where('is_archived', true)->get();
 
-        return view('user-management',compact('user','archivedUsers'));
+        return view('user-management', compact('user', 'archivedUsers'));
     }
 
-    public function AddUser(){
+    public function addUser(){
         return view('register', [
             'roles' => (new Role())->all(),
             'is_archived' => '0',
         ]);
     }
 
-
-    public function  SaveUser(Request $request){
+    public function saveUser(Request $request){
         try {
-        $request->validate([
-            'name'=> 'required',
-            'email'=>'required|email',
-            'year_of_registration'=>'required',
+            $request->validate([
+                'name'=> 'required',
+                'email'=>'required|email',
+                'year_of_registration'=>'required',
+                'password'=>'required|min:5|max:12',
+                'admission_number' => ['required', new ValidSuNumber],
+            ]);
 
-            'password'=>'required|min:5|max:12',
-           
-            'admission_number' => ['required',new ValidSuNumber],
-    
-        ]);
-           
+            $name=$request->name;
+            $email=$request->email;
+            $admission_number=$request->admission_number;
+            $year_of_registration=$request->year_of_registration;
+            $password=$request->password;
 
-        
+            $existingUser = User::where('admission_number', $admission_number)->first();
+            if ($existingUser) {
+                return redirect()->back()->with('error', 'User with this admission number already exists');
+            }
 
-        $name=$request->name;
-        $email=$request->email;
-    
-        $admission_number=$request->admission_number;
-        $year_of_registration=$request->year_of_registration;
-        $password=$request->password;
+            $user = new User;
+            $user->is_archived = 0;
+            $user->name = $name;
+            $user->email = $email;
+            $user->role_id = 2;
+            $user->admission_number = $admission_number;
+            $user->year_of_registration = $year_of_registration;
+            $user->password = $password;
 
-        $existingUser = User::where('admission_number', $admission_number)->first();
-        if ($existingUser) {
-            return redirect()->back()->with('error', 'User with this admission number already exists');
+            $user->save();
+            return redirect()->back()->with('success', 'user added successfully');
+        } catch (\Exception $e) {
+            return redirect()->back()->withErrors(['admission_number' => 'The admission number is not valid'])->withInput();
         }
-
-
-        $user = new User;
-        $user->is_archived=0;
-        $user->name=$name;
-        $user->email=$email;
-        $user->role_id=2;
-        $user->admission_number=$admission_number;
-        $user->year_of_registration=$year_of_registration;
-        $user->password=$password;
-
-        $user->save();
-        return redirect()->back()->with('success','user added succesfully');
-    } catch (\Exception $e) {
-      return redirect()->back()->withErrors(['admission_number' => 'The admission number is not valid'])->withInput();
-
-    }
-
     }
 
     public function editUser($id){
         $user = User::findOrFail($id);
-        return view('edit-user',compact('user'));
-
+        return view('edit-user', compact('user'));
     }
+
     public function updateUser(Request $request, $id){
- 
-        try{
-        $request->validate([
-            'name'=> 'required',
-            'email'=>'required|email',
-            'year_of_registration'=>'required',
-            'admission_number' => ['required',new ValidSuNumber],
-    
-        ]);
+        try {
+            $request->validate([
+                'name'=> 'required',
+                'email'=>'required|email',
+                'year_of_registration'=>'required',
+                'admission_number' => ['required', new ValidSuNumber],
+            ]);
 
+            $newName = $request->input('name');
+            $newEmail = $request->input('email');
+            $newAdmissionNumber = $request->input('admission_number');
+            $newYearOfRegistration = $request->input('year_of_registration');
 
+            User::where('id', '=', $id)->update([
+                'name' => $newName,
+                'email' => $newEmail,
+                'admission_number' => $newAdmissionNumber,
+                'year_of_registration' => $newYearOfRegistration,
+            ]);
 
-        $newName = $request->input('name');
-        $newEmail = $request->input('email');
-        $newAdmissionNumber = $request->input('admission_number');
-        $newYearOfRegistration = $request->input('year_of_registration');
-   
-
-         User::where('id', '=', $id)->update([
-         'name' => $newName,
-         'email' => $newEmail,
-         'admission_number' => $newAdmissionNumber,
-         'year_of_registration' => $newYearOfRegistration,
-        
-        ]);
-
-
-        return redirect('user-management')->with('success','user updated succesfully');
-    }catch(\Exception $e){
-        return redirect()->back()->withErrors(['admission_number' => 'The admission number is not valid'])->withInput();
-    }
+            return redirect('user-management')->with('success', 'user updated successfully');
+        } catch(\Exception $e) {
+            return redirect()->back()->withErrors(['admission_number' => 'The admission number is not valid'])->withInput();
+        }
     }
     
     public function deleteUser($id){
         User::where('id','=',$id)->delete();
-        return redirect('user-management')->with('success','user deleted succesfully');
-
-    }
-    public function index()
-    {
-
-    $role_id=Auth::user()->role_id;
-    if($role_id==1){ //role id is 1 for admin
-        return view('dashboard',[
-            'grades' => (new Grade())->all(),
-            'classes' => (new Classes())->all(),
-        ]);
-    }
-    else if($role_id==2){ //role id is 2 for management
-        return view('management',[
-            'notice' => (new Notice())->all(),
-        ]);
-    }
-    else if($role_id==3){ //role id is 3 for student
-        return view('student-panel',[
-            'notices'=>(new Notice())->all(),
-        ]);
-    }
-    else if ($role_id==4){ //role id is 4 for teacher
-        return view('teacher-panel',[
-            
-            'subjects'=>(new Subject())->all(),
-            'grades' => (new Grade())->all(),
-            'classes' => (new Classes())->all(),
-            'students'=>(new User())->all(),
-            'notices'=>(new Notice())->all(),
-        ]);
-    }
-    else{
-        return view('welcome');
-    }
+        return redirect('user-management')->with('success', 'user deleted successfully');
     }
 
-    public function logout(Request $request)
-    {
+    public function index(){
+        $role_id = Auth::user()->role_id;
+
+        if ($role_id == 1) { 
+            return view('dashboard', [
+                'grade' => (new Grade())->all(),
+                'classes' => (new Classes())->all(),
+            ]);
+        } else if ($role_id == 2) {
+            return view('management', [
+                'notice' => (new Notice())->all(),
+            ]);
+        } else if ($role_id == 3) {
+            return view('student-panel', [
+                'notices' => (new Notice())->all(),
+            ]);
+        } else if ($role_id == 4) {
+            return view('teacher-panel', [
+                'subjects' => (new Subject())->all(),
+                'grades' => (new Grade())->all(),
+                'classes' => (new Classes())->all(),
+                'students' => (new User())->all(),
+                'notices' => (new Notice())->all(),
+            ]);
+        } else {
+            return view('welcome');
+        }
+    }
+
+    public function logout(Request $request){
         Auth::logout();
-
         return view('/welcome');
     }
-    public function archiveUser($id) {//users are made inactive
+
+    public function archiveUser($id) {
         User::where('id', $id)->update([
             'is_archived' => true,
             'archived_at' => now(),
         ]);
-    
         return redirect('user-management')->with('success', 'User archived successfully');
     }
+
     public function archivedUsers() {
         $archivedUsers = User::where('is_archived', true)->get();
         return view('user-management', compact('archivedUsers'));
@@ -185,24 +160,13 @@ class UserController extends Controller
             'is_archived' => false,
             'archived_at' => null,
         ]);
-    
         return redirect('user-management')->with('success', 'User unarchived successfully');
     }
 
-    public function searchUsers(Request $request)
-    {
-    $searchTerm = $request->input('search');
+    public function searchUsers(Request $request){
+        $searchTerm = $request->input('search');
 
-    $user = User::where(function ($query) use ($searchTerm) {
-        $query->where('name', 'LIKE', "%$searchTerm%")
-            ->orWhere('email', 'LIKE', "%$searchTerm%")
-            ->orWhere('admission_number', 'LIKE', "%$searchTerm%")
-            ->orWhere('year_of_registration', 'LIKE', "%$searchTerm%");
-    })
-    ->get();
-
-    $archivedUsers = User::where('is_archived', true)
-        ->where(function ($query) use ($searchTerm) {
+        $user = User::where(function ($query) use ($searchTerm) {
             $query->where('name', 'LIKE', "%$searchTerm%")
                 ->orWhere('email', 'LIKE', "%$searchTerm%")
                 ->orWhere('admission_number', 'LIKE', "%$searchTerm%")
@@ -210,12 +174,20 @@ class UserController extends Controller
         })
         ->get();
 
-    return view('user-management', compact('user', 'archivedUsers'));
-   }
+        $archivedUsers = User::where('is_archived', true)
+            ->where(function ($query) use ($searchTerm) {
+                $query->where('name', 'LIKE', "%$searchTerm%")
+                    ->orWhere('email', 'LIKE', "%$searchTerm%")
+                    ->orWhere('admission_number', 'LIKE', "%$searchTerm%")
+                    ->orWhere('year_of_registration', 'LIKE', "%$searchTerm%");
+            })
+            ->get();
 
-public function showProfile(){
-    $user = Auth::user();
-    return view('profile/show',compact('user'));
-   }
-    
+        return view('user-management', compact('user', 'archivedUsers'));
+    }
+
+    public function showProfile(){
+        $user = Auth::user();
+        return view('profile/show', compact('user'));
+    }
 }
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,84 +1,58 @@
 <!DOCTYPE html>
-
 <html lang="en">
-    <head>
+<head>
     <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <script src="https://cdn.tailwindcss.com"></script>
-        <title>admin</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <script src="https://cdn.tailwindcss.com"></script>
+    <title>admin</title>
 </head>
-
 <body class="bg-white">
 
-
 <nav class="bg-gray-800">
-  <div class="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
-    <div class="relative flex h-16 items-center justify-between">
-
-      <div class="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
-       
-        <div class="hidden sm:ml-6 sm:block">
-          <div class="flex space-x-4">
-          
-            <a href="{{url ('student-list')}}" class="bg-gray-900 text-white rounded-md px-3 py-2 text-sm font-medium">Dashboard</a>
-        
-            <a href="{{ url ('user-management') }}" class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">User Management</a>
-       
-            <a href="{{ url('subject-list') }}"  class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">Course Management</a>
-            <a href="{{ url('add-teacher') }}"  class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">Enroll Teacher</a>
-            <a href="{{ url('add-student') }}"  class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium"> Enroll Student</a>
-            <a href="{{ url('profile/show') }}" class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">Profile</a>
-
-
-
-            <a href="{{ route('logout') }}" class="block mt-4 lg:inline-block lg:mt-0 text-amber-100 mr-4 px-3 py-2 no-underline hover:underline  sm:block"
-                                         @click.prevent="$root.submit();">
-                                    {{ __('Log Out') }}
-                                </a>
-
-
-
-
-
-
-
-          </div>
-        </div>
-      </div>
-
-        </div>
-      </div>
-    </div>
-  </div>
-
-
-</nav>
-
-</br>
-<p>Logged-in User: {{ auth()->user()->name }}</p>
-<div>
-<div>
-<a href="{{ url('add-grade') }}"  class="bg-amber-500 hover:bg-amber-700 text-white py-1 mb-6 px-3 rounded my-3 mt-1"> Add Grade</a>
-</div>
-<div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-    @foreach($grade as $grade)
-        <div class="bg-black text-white p-4 rounded-md shadow-md">
-            <p class="text-xs text-gray-500">Grade</p>
-            <p class="text-sm">{{ $grade->grade }}</p>
-
-            <div class="flex justify-end mt-3">
-    <a href="{{ route('classes.show', $grade) }}" class="bg-green-500 hover:bg-green-700 text-white py-1 px-3 rounded">View Classes</a>
-</div>
-
-            <div class="flex justify-end mt-3">
-                <a href="{{ url('edit-grade/'.$grade->id) }}" class="bg-blue-500 hover:bg-blue-700 text-white py-1 px-3 rounded">Edit</a>
-                <a href="{{ url('delete-grade/'.$grade->id) }}" class="bg-red-500 hover:bg-red-700 text-white py-1 px-3 rounded ml-2">Delete</a>
+    <div class="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+        <div class="relative flex h-16 items-center justify-between">
+            <div class="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
+                <div class="hidden sm:ml-6 sm:block">
+                    <div class="flex space-x-4">
+                        <a href="{{ url('student-list') }}" class="bg-gray-900 text-white rounded-md px-3 py-2 text-sm font-medium">Dashboard</a>
+                        <a href="{{ url('user-management') }}" class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">User Management</a>
+                        <a href="{{ url('subject-list') }}" class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">Course Management</a>
+                        <a href="{{ url('add-teacher') }}" class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">Enroll Teacher</a>
+                        <a href="{{ url('add-student') }}" class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">Enroll Student</a>
+                        <a href="{{ url('profile/show') }}" class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">Profile</a>
+                        <a href="{{ route('logout') }}" class="block mt-4 lg:inline-block lg:mt-0 text-amber-100 mr-4 px-3 py-2 no-underline hover:underline sm:block" @click.prevent="$root.submit();">
+                            {{ __('Log Out') }}
+                        </a>
+                    </div>
+                </div>
             </div>
         </div>
-    @endforeach
+    </div>
+</nav>
+
+<br>
+<p>Logged-in User: {{ auth()->user()->name }}</p>
+<div>
+    <div>
+        <a href="{{ url('add-grade') }}" class="bg-amber-500 hover:bg-amber-700 text-white py-1 mb-6 px-3 rounded my-3 mt-1">Add Grade</a>
+    </div>
+    <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+        @foreach($grade as $grade)
+            <div class="bg-black text-white p-4 rounded-md shadow-md">
+                <p class="text-xs text-gray-500">Grade</p>
+                <p class="text-sm">{{ $grade->grade }}</p>
+                <div class="flex justify-end mt-3">
+                    <a href="{{ route('classes.show', $grade) }}" class="bg-green-500 hover:bg-green-700 text-white py-1 px-3 rounded">View Classes</a>
+                </div>
+                <div class="flex justify-end mt-3">
+                    <a href="{{ url('edit-grade/'.$grade->id) }}" class="bg-blue-500 hover:bg-blue-700 text-white py-1 px-3 rounded">Edit</a>
+                    <a href="{{ url('delete-grade/'.$grade->id) }}" class="bg-red-500 hover:bg-red-700 text-white py-1 px-3 rounded ml-2">Delete</a>
+                </div>
+            </div>
+        @endforeach
+    </div>
 </div>
 
-</br>
-
+<br>
 </body>
 </html>
