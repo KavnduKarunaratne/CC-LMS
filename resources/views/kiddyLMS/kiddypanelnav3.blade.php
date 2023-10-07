f9a1f7718a89f1c9efa548fa91ec4deeff7121b6
SanduniSrikantha <CB010303@students.apiit.lk>
2023-10-07 22:28:08 +0530

    kiddy LMS UI

--- resources/views/kiddyLMS/kiddypanelnav3.blade.php
+++ resources/views/kiddyLMS/kiddypanelnav3.blade.php
@@ -56,7 +56,7 @@
    <div class="h-full px-3 pb-4 overflow-y-auto bg-white dark:bg-gray-800">
       <ul class="space-y-2 font-medium">
          <li class>
-            <a href="#" class="flex items-center p-2 text-indigo-600 hover:text-black rounded-lg dark:text-white hover:bg-indigo-300 dark:hover:bg-gray-700 group">
+            <a href="#" class="flex items-center p-2 text-indigo-600 hover:text-black rounded-lg dark:text-white bg-indigo-300 dark:hover:bg-gray-700 group">
                <svg class="w-10 h-10 text-indigo-600 transition duration-75 dark:text- group-hover:text-gray-900 dark:group-hover:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 21">
                   <path d="M16.975 11H10V4.025a1 1 0 0 0-1.066-.998 8.5 8.5 0 1 0 9.039 9.039.999.999 0 0 0-1-1.066h.002Z"/>
                   <path d="M12.5 0c-.157 0-.311.01-.565.027A1 1 0 0 0 11 1.02V10h8.975a1 1 0 0 0 1-.935c.013-.188.028-.374.028-.565A8.51 8.51 0 0 0 12.5 0Z"/>
@@ -65,7 +65,7 @@
             </a>
          </li>
          {{--<li>
-            <a href="{{ route('view-my-submissions') }}" class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-pink-300 dark:hover:bg-gray-700 group">
+            <a href="{{ route('view-my-submissions') }}" class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white bg-pink-300 dark:hover:bg-gray-700 group">
                <svg class="flex-shrink-0 w-10 h-10 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 18 18">
                   <path d="M6.143 0H1.857A1.857 1.857 0 0 0 0 1.857v4.286C0 7.169.831 8 1.857 8h4.286A1.857 1.857 0 0 0 8 6.143V1.857A1.857 1.857 0 0 0 6.143 0Zm10 0h-4.286A1.857 1.857 0 0 0 10 1.857v4.286C10 7.169 10.831 8 11.857 8h4.286A1.857 1.857 0 0 0 18 6.143V1.857A1.857 1.857 0 0 0 16.143 0Zm-10 10H1.857A1.857 1.857 0 0 0 0 11.857v4.286C0 17.169.831 18 1.857 18h4.286A1.857 1.857 0 0 0 8 16.143v-4.286A1.857 1.857 0 0 0 6.143 10Zm10 0h-4.286A1.857 1.857 0 0 0 10 11.857v4.286c0 1.026.831 1.857 1.857 1.857h4.286A1.857 1.857 0 0 0 18 16.143v-4.286A1.857 1.857 0 0 0 16.143 10Z"/>
                </svg>
@@ -74,7 +74,7 @@
             </a>
          </li>--}}
          <li>
-            <a href="{{ route('view-my-submissions') }}" class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-pink-200 dark:hover:bg-gray-700 group">
+            <a href="{{ route('view-my-submissions') }}" class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white bg-pink-200 dark:hover:bg-gray-700 group">
                <svg class="flex-shrink-0 w-10 h-10 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
                   <path d="M5 5V.13a2.96 2.96 0 0 0-1.293.749L.879 3.707A2.96 2.96 0 0 0 .13 5H5Z"/>
                   <path d="M6.737 11.061a2.961 2.961 0 0 1 .81-1.515l6.117-6.116A4.839 4.839 0 0 1 16 2.141V2a1.97 1.97 0 0 0-1.933-2H7v5a2 2 0 0 1-2 2H0v11a1.969 1.969 0 0 0 1.933 2h12.134A1.97 1.97 0 0 0 16 18v-3.093l-1.546 1.546c-.413.413-.94.695-1.513.81l-3.4.679a2.947 2.947 0 0 1-1.85-.227 2.96 2.96 0 0 1-1.635-3.257l.681-3.397Z"/>
@@ -93,7 +93,7 @@
             </a>
          </li>--}}
          <li>
-            <a href="{{ url('profile/show') }}" class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-green-200 dark:hover:bg-gray-700 group">
+            <a href="{{ url('profile/show') }}" class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white bg-green-200 dark:hover:bg-gray-700 group">
                <svg class="flex-shrink-0 w-10 h-10 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 18">
                   <path d="M14 2a3.963 3.963 0 0 0-1.4.267 6.439 6.439 0 0 1-1.331 6.638A4 4 0 1 0 14 2Zm1 9h-1.264A6.957 6.957 0 0 1 15 15v2a2.97 2.97 0 0 1-.184 1H19a1 1 0 0 0 1-1v-1a5.006 5.006 0 0 0-5-5ZM6.5 9a4.5 4.5 0 1 0 0-9 4.5 4.5 0 0 0 0 9ZM8 10H5a5.006 5.006 0 0 0-5 5v2a1 1 0 0 0 1 1h11a1 1 0 0 0 1-1v-2a5.006 5.006 0 0 0-5-5Z"/>
                </svg>
@@ -109,7 +109,7 @@
             </a>
          </li>-->
          <li>
-            <a href="{{ route('logout') }}" class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-yellow-200 dark:hover:bg-gray-700 group">
+            <a href="{{ route('logout') }}" class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white bg-yellow-200 dark:hover:bg-gray-700 group">
                <svg class="flex-shrink-0 w-10 h-10 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 16">
                   <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 8h11m0 0L8 4m4 4-4 4m4-11h3a2 2 0 0 1 2 2v10a2 2 0 0 1-2 2h-3"/>
                </svg>
@@ -139,10 +139,7 @@
                 <!DOCTYPE html>
 
 <div class="image-container">
-  <img src="images/cloudbg.png" alt="Image 1" class=" move-left">
-  <img src="images/cloud.png" alt="Image 2" class="move-right">
-  <img src="images/cloudbg.png" alt="Image 1" class=" move-left1">
-  
+  @include('kiddyLMS.carousel')
 </div>
     
 
@@ -185,6 +182,7 @@
 <div class="bg-white py-5 mb-0">
     <div class="container mx-auto px-4">
         <h2 class="text-2xl font-semi-bold text-gray-900 mb-8">Here are Your Subjects!</h2>
+        @include('kiddyLMS.subjects')
         <div class="grid grid-cols-1 md:grid-cols-1 gap-8">
             <div class="bg-white rounded-lg border-solid border-2 border-black p-8">
                 <div class="relative overflow-hidden">
@@ -216,7 +214,7 @@
 </div>--}}
 <!--Notices-->
 <div class="bg-white py-5 mb-10">
-    <h2 class="text-2xl font-semi-bold text-gray-900 ml-4 mb-8">Accouncements</h2>
+    <h2 class="text-2xl font-semi-bold text-gray-900 ml-4 mb-8">Announcements</h2>
     @if ($notices->count() > 0)
     @foreach ($notices as $notice)
     @if ($notice->grade_id == null)
@@ -299,93 +297,6 @@
 
 
 <!--USING-->
-
-
-<!DOCTYPE html>
-<html>
-<head>
-<style>
-  .image-container {
-    width: 100%;
-    height:80%;
-    overflow: hidden;
-    white-space: nowrap;
-  }
-
-  .image-container img {
-    display: inline-block;
-    animation-timing-function: linear;
-    animation-iteration-count: infinite;
-    /*animation-direction: alternate;*/
-    
-  }
-
-  .move-left{
-    height:70%;
-    width:auto;
-  }
-
-  .move-left1{
-    height:70%;
-    width:auto;
-  }
-
-  .move-right{
-    height:70%;
-    width:auto;
-  }
-
-  .move-left {
-    animation-name: moveLeft;
-    animation-duration: 15s; /* Adjust the animation duration for slower movement */
-  }
-
-  .move-right {
-    animation-name: moveRight;
-    animation-duration: 20s; /* Adjust the animation duration for slower movement */
-  }
-
-  .move-left1 {
-    animation-name: moveLeft;
-    animation-duration: 15s; /* Adjust the animation duration for slower movement */
-  }
-
-
-
-  @keyframes moveLeft {
-    0% {
-      transform: translateX(-80%); /* Start from the left */
-    }
-    100% {
-      transform: translateX(80%); /* Move to the right */
-    }
-  }
-
-  @keyframes moveLeft1 {
-    0% {
-      transform: translateX(-80%); /* Start from the left */
-    }
-    100% {
-      transform: translateX(80%); /* Move to the right */
-    }
-  }
-
-  @keyframes moveRight {
-    0% {
-      transform: translateX(100%); /* Start from the right */
-    }
-    100% {
-      transform: translateX(-200%); /* Move to the left */
-    }
-  }
-</style>
-</head>
-<body>
-
-
-
-</body>
-</html>
 
 <!--<!DOCTYPE html>
 <html>
